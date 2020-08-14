a56201d7bf9fb1b39868e732af39a16cfea5ec65
kakim <kakim@com2us.com>
2020-08-14 12:20:55 +0900

    commit

--- docker/start.sh
+++ docker/start.sh
@@ -16,7 +16,7 @@ then
 else
     # 디렉토리가 있는 경우 pull
     git pull "${gitUrl}" "${sourceRoot}"
-fI
+fi
 
 # 3. 프로젝트 폴더로 이동
 cd "${sourceRoot}"
@@ -29,27 +29,6 @@ go mod download
 # 5. 프로젝트 빌드
 go build -o "${projectName}" ./elf/app/get.started
 
-ln -s /build/calc /bin/
+#ln -s /build/calc /bin/
 
 
-
-
-
-#WORKDIR /build
-#WORKDIR /bin
-#
-### 전체 표준 라이브러리를 다시 빌드
-##ENV CGO_ENABLED=0
-#
-#COPY go.mod ./
-#RUN go mod download
-#
-#COPY . ./
-#RUN go build -o calc ./app/main.go
-#
-## Run on scratch
-#FROM alpine:latest
-#
-#COPY --from=builder /build/calc /bin/
-#
-#EXPOSE 8000
